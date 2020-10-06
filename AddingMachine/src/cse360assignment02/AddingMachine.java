3ae6adec630d11dade436bc6584d98ce4b004067
Nick <nmeyer5@asu.edu>
2020-10-06 14:06:49 -0700

    Initial Commit!

---
+++ AddingMachine/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,33 @@
+package cse360assignment02;
+//This class is an adding machine
+
+public class AddingMachine {
+  private int total;
+  
+  //Constructor
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  //accessor method for the total
+  public int getTotal () {
+    return 0;
+  }
+  
+  //adds the input
+  public void add (int value) {
+  }
+
+  //subtracts the input
+  public void subtract (int value) {
+  }
+
+  //creates a string and returns it
+  public String toString () {
+    return "";
+  }
+
+  //method to clear
+  public void clear() {
+  }
+}
